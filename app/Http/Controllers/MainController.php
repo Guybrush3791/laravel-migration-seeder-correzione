2b06ff87e3317e1a76ed6c3442546867d8657234
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2024-02-14 10:25:19 +0100

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,10 +10,7 @@ class MainController extends Controller
 {
     public function index() {
 
-        // dd(now() -> toDateString());
         $trains = Train :: whereDate('start_time', '=', now() -> toDateString()) -> get();
-        // $trains = Train :: all();
-        // dd($trains);
 
         return view('pages.index', compact('trains'));
     }
